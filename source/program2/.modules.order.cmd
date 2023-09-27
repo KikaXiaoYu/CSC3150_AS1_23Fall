e773ae41651b535d43fb35d921d98ab8c784489c
ShinoXiaoyu <shinoxiaoyu@outlook.com>
2023-09-27 16:36:49 +0000

    make program2

---
+++ source/program2/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vagrant/csc3150/Assignment_1_121090642/source/program2/modules.order := {   echo /home/vagrant/csc3150/Assignment_1_121090642/source/program2/program2.ko; :; } | awk '!x[$$0]++' - > /home/vagrant/csc3150/Assignment_1_121090642/source/program2/modules.order
---
+++ source/program2/program2.mod
@@ -0,0 +1,2 @@
+/home/vagrant/csc3150/Assignment_1_121090642/source/program2/program2.o
+
